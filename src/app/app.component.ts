ee060027244aaf69b13e40b20db3bf94ce951f1f
shameedshamee <babushameed44@gmail.com>
2024-05-30 09:59:30 +0530

    updates on 30/05/24 which is on data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PostComponent } from "./post/post.component";
 import { NavbarbasicComponent } from "./navbarbasic/navbarbasic.component";
+import { PostListComponent } from './post-list/post-list.component';
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, PostComponent, NavbarbasicComponent]
+    imports: [RouterOutlet, PostComponent, NavbarbasicComponent, PostListComponent]
 })
 export class AppComponent {
   title = 'my-angular-app';
+  parentMessage: string = "Messing coming from parent component"
 }
--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,14 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import { PostListComponent } from '../post-list/post-list.component';
 @Component({
   selector: 'app-post',
   standalone: true,
-  imports: [],
   templateUrl: './post.component.html',
-  styleUrl: './post.component.css'
+  styleUrl: './post.component.css',
+  imports: [PostListComponent],
 })
 export class PostComponent implements OnInit {
 
+  title:string = "List of Posts"
+  massagePost:string = "Transfer data throw Data Binding and components relations"
+  
+  postParentMessage:string = "Message coming from the post parent"
+  @Input() fromParent!: string ;
+  
   constructor(){}
 
   ngOnInit(): void {
