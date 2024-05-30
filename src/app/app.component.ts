86579a0f064034765df7533be88ab46b2f0469dd
shameedshamee <babushameed44@gmail.com>
2024-05-31 01:20:53 +0530

    updates on 30/05/24 which is on input and output decarator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,31 @@
-import { Component } from '@angular/core';
+import { Component , ViewChild, AfterViewInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PostComponent } from "./post/post.component";
 import { NavbarbasicComponent } from "./navbarbasic/navbarbasic.component";
-import { PostListComponent } from './post-list/post-list.component';
+// import { PostListComponent } from './post-list/post-list.component';
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
-    styleUrl: './app.component.css',
-    imports: [RouterOutlet, PostComponent, NavbarbasicComponent, PostListComponent]
+    styleUrls: ['./app.component.css'],
+    imports: [RouterOutlet, PostComponent, NavbarbasicComponent]
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'my-angular-app';
-  parentMessage: string = "Messing coming from parent component"
+  parentMessage: string = "Messing coming from parent component which is main Parent"
+  message!:string;
+  fromChildOutput!: string;
+  @ViewChild(PostComponent) childComp: any;
+
+  constructor() {
+    console.log(this.childComp);
+  }
+  ngAfterViewInit(): void {
+    console.log(this.childComp);
+    this.message = this.childComp.messagePost
+  }
+
+  reciveMessage($event: any){
+  this.fromChildOutput = $event;
+  }
 }
--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,23 +1,31 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { PostListComponent } from '../post-list/post-list.component';
 @Component({
   selector: 'app-post',
   standalone: true,
   templateUrl: './post.component.html',
-  styleUrl: './post.component.css',
+  styleUrls: ['./post.component.css'],
   imports: [PostListComponent],
 })
 export class PostComponent implements OnInit {
 
   title:string = "List of Posts"
-  massagePost:string = "Transfer data throw Data Binding and components relations"
+  messagePost:string = "Transfer data throw Data Binding and components relations"
   
   postParentMessage:string = "Message coming from the post parent"
+  
+  outputChildMessage: string = "Message from  Child Component Via Output";
+  
   @Input() fromParent!: string ;
   
+  @Output() messageEvent = new EventEmitter<string>();
   constructor(){}
 
   ngOnInit(): void {
     
+  }
+
+  sendMessage(){
+    this.messageEvent.emit(this.outputChildMessage);
   }
 }
