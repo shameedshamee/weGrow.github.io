ef7b16b84c892d30665032195598631b63f736fc
shameedshamee <babushameed44@gmail.com>
2024-06-14 01:45:26 +0530

    New Changes on 13-06-2024

--- README.md
+++ README.md
@@ -25,3 +25,12 @@ Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To u
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+
+
+## Angular.Json file 
+
+{
+  "cli": {
+    "analytics": "b37e4db5-8a58-4c9d-aa47-7f63b9472997"
+  }
+}
--- src/app/practice-block/practice-block.component.ts
+++ src/app/practice-block/practice-block.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms'
+
+// interface Post {
+//   id: number;
+//   postName: string;
+// }
 
 @Component({
   selector: 'app-practice-block',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './practice-block.component.html',
   styleUrl: './practice-block.component.css'
 })
@@ -11,9 +18,59 @@ export class PracticeBlockComponent {
   message: string = "Message From Typestript Component File"
   imgUrl: string = 'https://logowik.com/content/uploads/images/angular-new6082.logowik.com.webp'
 
+  postTitle!: string;
+  postDetails!: string;
+  imageURL!: string;
+  postURL!: string;
+  addBackground!: boolean;
+
   bool:boolean = false;
   buttonClick(){
     this.bool = !this.bool;
     // console.log(this.bool);
+  }
+
+  // onKeyup(username: any){
+  //   console.log(username)
+  // }
+  usernames: string[] = [];
+
+  // onKeyup(username: string): void {
+  //   if (username.trim()) {
+  //     this.usernames.push(username.trim());
+  //   }
+  // }
+
+  onKeyup(username: string, inputElement: HTMLInputElement): void {
+    if (username.trim()) {
+      this.usernames.push(username.trim());
+      inputElement.value = ''; // Clear the input field after adding the username
+    }
+  }
+
+  deleteUsername(index: number): void {
+    this.usernames.splice(index, 1);
+  }
+
+  postArray:Array<string> = ['Post 1','Post 2','Post 3','Post 4','Post 5'];
+
+  objArray:Array<any> = [
+    { id: 1, postName: 'Post Name 1'},
+    { id: 2, postName: 'Post Name 2'},
+    { id: 3, postName: 'Post Name 3'},
+    { id: 4, postName: 'Post Name 4'},
+    { id: 5, postName: 'Post Name 5'},
+  ]
+
+  addNew(){
+    this.objArray.push({ id: 6, postName: 'Post Name 6'});
+  }
+  // // onDelete(post: Post): void {
+  //   // let index =this.objArray.indexOf(post);
+  //   this.objArray.splice(index, 1);
+  // }
+  onDelete(index: number): void {
+    // let index =this.objArray.indexOf(post);
+    this.objArray.splice(index, 1);
   }
 }
